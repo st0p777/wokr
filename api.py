4751b4c308368aaa91592c664776a224b4b51a3a
st0p777 <89073298+st0p777@users.noreply.github.com>
2021-08-17 20:04:36 +0300

    Add files via upload

---
+++ api.py
@@ -0,0 +1,36 @@
+from flask import request, jsonify, Flask
+import flask
+import traceback
+import pickle
+import pandas as pd
+
+app = Flask(__name__)
+with open('model.pkl', 'rb') as file:
+    classifier = pickle.load(file)
+with open('model_columns.pkl', 'rb') as f:
+    model_columns = pickle.load(f)
+
+
+@app.route('/')
+def welcome():
+    return "Boston Housing Price Prediction"
+
+
+@app.route('/predict', methods=['POST', 'GET'])
+def predict():
+    if flask.request.method == 'GET':
+        return "Prediction page"
+    else:
+        try:
+            json_ = request.json
+            print(json_)
+            query_ = pd.get_dummies(pd.DataFrame(json_))
+            query = query_.reindex(columns=model_columns, fill_value=0)
+            prediction = list(classifier.predict(query))
+            return jsonify({"prediction": str(prediction)})
+        except:
+            return jsonify({"trace": traceback.format_exc()})
+
+
+if __name__ == "__main__":
+    app.run()
